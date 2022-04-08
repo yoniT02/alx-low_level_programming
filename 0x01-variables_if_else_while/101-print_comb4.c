fda680341062242be789c4bcf44f046fabf0fa28
Yoni Boss <yonastaye859@gmail.com>
2022-04-08 21:08:05 +0300

    second to last question

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int j;
+	int i;
+	int n = 44;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (k = (i + 1); i < 57; i++)
+		{
+			for (j = (k + 1); j < 58; j++)
+			{
+				if (k == j)
+				{
+					;
+				}
+				else
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (i < 56)
+					{
+						putchar(n);
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
