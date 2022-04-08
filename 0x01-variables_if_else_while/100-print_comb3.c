f9dc50409d95277c75ea83ee2d02e955f4bb079c
Yoni Boss <yonastaye859@gmail.com>
2022-04-08 19:59:47 +0300

    question advanced

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int j;
+	int i;
+	int n = 44;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = i; j < 58; j++)
+		{
+			if (i == j)
+				;
+			else if
+			{
+				putchar(i);
+				putchar(j);
+				putchar(n);
+				putchar(' ');
+			}
+		}
+	}
+	return (0);
+}
+
