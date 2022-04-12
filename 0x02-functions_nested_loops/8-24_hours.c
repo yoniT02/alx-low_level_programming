f1cc489e9639ac50d8331337fd4d5016a967701a
Yoni Boss <yonastaye859@gmail.com>
2022-04-13 00:19:48 +0300

    question 8 maybe

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints the time from 00:00 to 23:59
+ *
+ * Return: void.
+ */
+void jack_bauer(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			for (k = 0; k < 6; k++)
+			{
+				for (l = 0; l < 10; l++)
+				{
+					_putchar(i + '0');
+					_putchar(j + '0');
+					_putchar(':');
+					_putchar(k + '0');
+					_putchar(l + '0');
+					_putchar('\n');
+            			}
+         		}
+		if (j == 3 )
+			break;
+		}
+	}
+}
