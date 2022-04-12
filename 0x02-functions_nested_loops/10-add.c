2dc7c78cf2e3a2ba08d7fe2af3b821f342598c6b
Yoni Boss <yonastaye859@gmail.com>
2022-04-13 01:01:58 +0300

    this shit sucks question 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two inegers
+ * @a: integer number one
+ * @b: integer number two
+ *
+ * Return: the sum of the numbers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
