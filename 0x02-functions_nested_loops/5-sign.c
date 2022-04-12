ff0fbccf5af1b34a49c9e7ddd8d934fd466ba84e
Yoni Boss <yonastaye859@gmail.com>
2022-04-12 22:39:42 +0300

    quetstion 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - checks if the characters are 0,1,-1
+ * @n: The character to check
+ *
+ * Return: On success 1.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+}
