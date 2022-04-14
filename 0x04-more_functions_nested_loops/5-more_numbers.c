9ebff2d98a28d15affdefbfd9efa2300e7d6fd40
Yoni Boss <yonastaye859@gmail.com>
2022-04-14 20:37:45 +0300

    few questions done

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,12 +10,11 @@ void more_numbers(void)
 	int i;
 	int j;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 15; j++)
 			_putchar(j + '0');
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
 
