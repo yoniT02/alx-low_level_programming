cfa6eaead748a9050c39ecc9fe3cde218d9468f9
Yoni Boss <yonastaye859@gmail.com>
2022-04-14 23:02:13 +0300

    question 9 I think

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+			i++;
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+			i++;
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+			i++;
+		}
+		printf("%d", i);
+		printf(" ");
+	}
+	return (0);
+}
