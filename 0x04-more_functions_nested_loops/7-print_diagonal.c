2a00b9f8a9971e958649de32d033bc2cbadd7c76
Yoni Boss <yonastaye859@gmail.com>
2022-04-14 22:18:19 +0300

    question number 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints the '\' character n times
+ * @n: the integer that determines the execution of the for loop
+ *
+ * Return: void.
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
