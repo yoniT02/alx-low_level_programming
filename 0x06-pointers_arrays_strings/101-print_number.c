89ac97fc90a3fdf9821c781a13bb2c156424a895
Yoni Boss <yonastaye859@gmail.com>
2022-04-21 17:42:13 +0300

    new commit on question 10

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,28 +1,42 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- *main - Entry point
+ * print_number - prints integers
  *
- * Return: Always 0 (Success)
+ * @n: accpets integer
+ * Return - void
  */
-int main(void)
+void print_number(int n)
 {
-	int n;
-	int a[5];
-	int *p;
-
-	a[2] = 1024;
-	p = &n;
-	/*
-	* write your line of code here...
-	* Remember:
-	* - you are not allowed to use a
-	* - you are not allowed to modify p
-	* - only one statement
-	* - you are not allowed to code anything else than this line of code
-	*/
-	;
-	/* ...so that this prints 98\n */
-	printf("a[2] = %d\n", a[2]);
-	return (0);
+	if (n > 999)
+	{
+		_putchar(n / 1000 + '0');
+		n % = 1000;
+		_putchar(n / 100 + '0');
+		n % = 100;
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+	else if (n > 99)
+	{
+		_putchar(n / 100 + '0');
+		n % = 100;
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+	else if (n > 9)
+	{
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+	else if (n < 0)
+	{
+		n * = -1;
+		_putchar('-');
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+	else
+	_putchar('0');
 }
+
