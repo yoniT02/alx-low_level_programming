d111b3e9584592fe263847f0335e04c9d48b38dd
Yoni Boss <yonastaye859@gmail.com>
2022-04-21 22:33:12 +0300

    fix for question 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -12,7 +12,7 @@ int main(void)
 	int a[5];
 	int *p;
 
-	a[2] = 1024;
+	a[2] = 98;
 	p = &n;
 	/*
 	* write your line of code here...
@@ -22,7 +22,7 @@ int main(void)
 	* - only one statement
 	* - you are not allowed to code anything else than this line of code
 	*/
-	return(printf("a[2] = 98\n"));
+	;
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
